fa3051d7a34854ed3d394eeaa0291c8ecb2ab74a
Siran Cao <79330375+siranchao@users.noreply.github.com>
2023-05-26 16:55:54 -0400

    Add files via upload

---
+++ WS07/lab/Dumper.cpp
@@ -0,0 +1,18 @@
+/*
+Name:
+Email:
+Student ID:
+Data:
+Section:
+*/
+#include<iostream>
+#include "Dumper.h"
+#include "VehicleBasic.h"
+
+namespace sdds
+{
+	
+	
+	
+	
+}
---
+++ WS07/lab/Dumper.h
@@ -0,0 +1,20 @@
+/*
+Name:
+Email:
+Student ID:
+Data:
+Section:
+*/
+#ifndef SDDS_Dumper_H
+#define SDDS_Dumper_H
+#include <iostream>
+#include "VehicleBasic.h"
+
+namespace sdds
+{
+	
+	
+	
+	
+}
+#endif
