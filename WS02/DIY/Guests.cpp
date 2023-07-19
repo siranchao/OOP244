c086a375bf8df6b9b44ca74d6928371b83a020b6
Siran Cao <79330375+siranchao@users.noreply.github.com>
2023-07-19 15:09:28 -0400

    Delete WS02 directory

--- WS02/DIY/Guests.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-namespace sdds {
-   void read(char* str, int len) {
-      if (cin.peek() == '\n') cin.ignore(); // checks the incomming character, if it is newline, it will remove it
-      cin.getline(str, len);
-   }
-}
