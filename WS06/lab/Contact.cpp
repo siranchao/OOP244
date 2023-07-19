679167bc26d695639a513510b02e9a61a77609b7
Siran Cao <79330375+siranchao@users.noreply.github.com>
2023-07-19 15:09:59 -0400

    Delete WS06 directory

--- WS06/lab/Contact.cpp
+++
@@ -1,8 +0,0 @@
-#define  _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <cstring>
-#include "Contact.h"
-using namespace std;
-namespace sdds {
- 
-}
--- WS06/lab/Contact.h
+++
@@ -1,9 +0,0 @@
-#ifndef SDDS_CONTACT_H
-#define SDDS_CONTACT_H
-#include <iostream>
-namespace sdds {
-
-}
-#endif // !SDDS_CONTACT_H
-
-
